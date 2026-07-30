9511ec7b5f96f791bbb3c6cc1922afd2876e838c
eduardo38oitao <klen.soares@escola.pr.gov.br>
2026-07-30 08:09:53 -0300

    Add files via upload

--- modelo.py
+++ modelo.py
@@ -1,56 +1,52 @@
-#Filmes e séries tem as seguintes características;
-
-#Filme: Nome, Ano, Duração, curir
-#Séries: Nome, Ano, Temporadas, curtir
-
-class Filmes:
-    def __init__(self,nome,ano,duracao):
-        self.__nome = nome.title()
+# Classe mãe/principal
+# Super Classe
+class Programas:
+    def __init__(self, nome, ano):
+        self._nome = nome.title()
         self.ano = ano
+        self._curtidas = 0
+
+    @property
+    def curtidas(self):
+        return self._curtidas
+
+    @property
+    def nome(self):
+        return self._nome
+    
+    # Removido o @property, pois curtir é uma ação (método)
+    def curtir(self):
+        self._curtidas += 1
+
+    def __str__(self):
+        return f"{self.nome} - {self.ano} - {self._curtidas} Curtida"
+
+class Filmes(Programas):
+    def __init__(self, nome, ano, duracao):
+        # Chama o construtor da classe mãe para criar o nome e as curtidas
+        super().__init__(nome, ano)
         self.duracao = duracao
-        self.__curtir =  0
 
-    @property
-    def valor_curtir(self):
-        return self.__curtir
-    
-    @property
-    def valor_nome(self):
-        return self.__nome
+    def __str__(self):
+        return f"{self.nome} - {self.ano} - {self.duracao } Minutos - {self._curtidas} Curtidas"
 
-    def curtida(self):
-        self.curtir += 1 
-
-
-
-class Series:
-    def __init__(self,nome,ano,temporadas):
-        self.nome = nome.title()
-        self.ano = ano
+class Series(Programas):
+    def __init__(self, nome, ano, temporadas):
+        # Chama o construtor da classe mãe para criar o nome e as curtidas
+        super().__init__(nome, ano)
         self.temporadas = temporadas
-        self.curtir =  0
 
-    @property
-    def valor_curtir(self):
-        return self.__curtir
-    
-    @property
-    def valor_nome(self):
-        return self.__nome
+    def __str__(self):
+        return f"{self.nome} - {self.ano} - {self.temporadas} Temporadas - {self._curtidas} Curtidas"
 
-    
-    def curtida(self):
-        self.curtir += 1 
+# Criando os objetos
+avatar = Filmes("avatar o caminho da água", 2022, 192)
+avatar.curtir()  # Agora funciona corretamente
 
+serie1 = Series("heartbreak high", 2022, 3)
+serie1.curtir()  # Agora funciona corretamente
 
+filmes_series = [avatar, serie1]
 
-
-heartbreak_high = Series("Heartbreak High", 2022, 3)
-heartbreak_high.curtida()
-
-print(f"Nome: {heartbreak_high.nome} - Ano: {heartbreak_high.ano} - Temporada: {heartbreak_high.temporadas} - {heartbreak_high.curtir}")
-
-
-avatar = Filmes("Avatar", 2009, 177)
-avatar.curtida()
-print(f"Nome: {avatar.nome} - Ano: {avatar.ano} - Duracao: {avatar.duracao} - {avatar.curtir}")
+for programas in filmes_series:
+    print(programas)
